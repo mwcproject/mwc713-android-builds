58681365d4e8cad9e570f213b85c266d0ef6eff1
bayk <konstantin.bay@gmail.com>
2020-06-18 14:22:25 -0700

    add mwczip to upload

--- deploy_linux_x32.sh
+++ deploy_linux_x32.sh
@@ -8,27 +8,32 @@ chmod 400 ./uploader.pem
 TAG_FOR_BUILD_FILE=mwc713.version
 if [ -f "$TAG_FOR_BUILD_FILE" ]; then
 VERSION=`cat $TAG_FOR_BUILD_FILE`
-DPKG_NAME=mwc713_$VERSION
+MWC713_NAME=mwc713_$VERSION
+MWCZIP_NAME=mwczip_$VERSION
 else
-DPKG_NAME=mwc713_3.2.beta.$1
+MWC713_NAME=mwc713_3.2.beta.$1
+MWCZIP_NAME=mwczip_3.2.beta.$1
 fi
 
 
 function upload_file {
-    TARGET=$1 
-    BIN_SUFFIX=$2
-    PASS=$3
+    DPKG_NAME=$1
+    BIN_FN=$2
+    TARGET=$3
+    BIN_SUFFIX=$4
+    PASS=$5
 
     BIN_NAME=${DPKG_NAME}_${BIN_SUFFIX}
-	cp mwc713/target/${TARGET}/release/mwc713 $BIN_NAME
+	cp mwc713/target/${TARGET}/release/${BIN_FN} $BIN_NAME
 	echo "md5sum = `md5sum $BIN_NAME`";
 echo "pass = $PASS"
         ./scp.expect $BIN_NAME $PASS
 }  
 
-
 mkdir -p ~/.ssh
 
-upload_file arm-linux-androideabi arm $2
-upload_file i686-linux-android x86 $2
+upload_file $MWC713_NAME mwc713 arm-linux-androideabi arm $2
+upload_file $MWC713_NAME mwc713 i686-linux-android x86 $2
 
+upload_file $MWCZIP_NAME mwczip arm-linux-androideabi arm $2
+upload_file $MWCZIP_NAME mwczip i686-linux-android x86 $2
--- deploy_linux_x64.sh
+++ deploy_linux_x64.sh
@@ -8,19 +8,23 @@ chmod 400 ./uploader.pem
 TAG_FOR_BUILD_FILE=mwc713.version
 if [ -f "$TAG_FOR_BUILD_FILE" ]; then
 VERSION=`cat $TAG_FOR_BUILD_FILE`
-DPKG_NAME=mwc713_$VERSION
+MWC713_NAME=mwc713_$VERSION
+MWCZIP_NAME=mwczip_$VERSION
 else
-DPKG_NAME=mwc713_3.2.beta.$1
+MWC713_NAME=mwc713_3.2.beta.$1
+MWCZIP_NAME=mwczip_3.2.beta.$1
 fi
 
 
 function upload_file {
-    TARGET=$1
-    BIN_SUFFIX=$2
-    PASS=$3
+    DPKG_NAME=$1
+    BIN_FN=$2
+    TARGET=$3
+    BIN_SUFFIX=$4
+    PASS=$5
 
     BIN_NAME=${DPKG_NAME}_${BIN_SUFFIX}
-	cp mwc713/target/${TARGET}/release/mwc713 $BIN_NAME
+	cp mwc713/target/${TARGET}/release/${BIN_FN} $BIN_NAME
 	echo "md5sum = `md5sum $BIN_NAME`";
 echo "pass = $PASS"
         ./scp.expect $BIN_NAME $PASS
@@ -29,5 +33,8 @@ echo "pass = $PASS"
 
 mkdir -p ~/.ssh
 
-upload_file aarch64-linux-android arm64 $2
-upload_file x86_64-linux-android x86_64 $2
+upload_file $MWC713_NAME mwc713 aarch64-linux-android arm64 $2
+upload_file $MWC713_NAME mwc713 x86_64-linux-android x86_64 $2
+
+upload_file $MWCZIP_NAME mwczip arm-linux-androideabi arm $2
+upload_file $MWCZIP_NAME mwczip i686-linux-android x86 $2
